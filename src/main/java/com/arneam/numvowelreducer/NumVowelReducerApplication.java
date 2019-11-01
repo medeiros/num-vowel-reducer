af18a93e86b5d01938000283d88982debca868bb
Daniel Medeiros <daniel@arneam.com>
2019-11-01 05:08:55 -0300

    Add Eureka client capabilities

--- src/main/java/com/arneam/numvowelreducer/NumVowelReducerApplication.java
+++ src/main/java/com/arneam/numvowelreducer/NumVowelReducerApplication.java
@@ -2,8 +2,10 @@ package com.arneam.numvowelreducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NumVowelReducerApplication {
 
 	public static void main(String[] args) {

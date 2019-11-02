c5f17cd52971bbf13aa0eb40a0a58fbc9678b95f
Daniel Medeiros <daniel@arneam.com>
2019-11-02 10:00:38 -0300

    Add Log4j2/Kafka integration

--- src/main/java/com/arneam/numvowelreducer/application/VowelReducerController.java
+++ src/main/java/com/arneam/numvowelreducer/application/VowelReducerController.java
@@ -2,12 +2,14 @@ package com.arneam.numvowelreducer.application;
 
 import com.arneam.numvowelreducer.domain.VowelReducer;
 import com.arneam.numvowelreducer.infrastructure.TextReducerClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("reduce")
+@Slf4j
 public class VowelReducerController {
 
   @Autowired
@@ -16,6 +18,7 @@ public class VowelReducerController {
   @GetMapping("/{name}")
   @ResponseStatus(HttpStatus.OK)
   public int reduceToNumber(@PathVariable String name) {
+    log.info("calling 'VowelReducerController#reduceToNumber' with parameter: " + name);
     return textReducerClient.reduceToNumber(VowelReducer.instance().reduceToVowels(name));
   }
 
